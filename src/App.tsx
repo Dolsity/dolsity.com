6d48a0868f1a8bf653a0350d504c3769589f0c4f
Jay <Acorder43812@gmail.com>
2025-09-14 00:36:41 -0700

    Add Navigator component and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { CssBaseline } from '@mui/material'
+
+import Navigator from './components/Navigation/Navigator'
 
 export default function App() {
   return (
-    <>Hello World!</>
+    <>
+      <CssBaseline />
+      <Navigator />
+    </>
   )
 }
-
